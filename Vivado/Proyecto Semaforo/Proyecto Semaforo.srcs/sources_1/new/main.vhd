fe6f30bfe71322819cc23b0d1a4d642f258c13f0
Axel Rodriguez <alejandrom9712@gmail.com>
2019-09-18 16:37:13 -0600

    Proyecto de semaforo con semaforo por defecto funcionando sin parpadeo

--- Vivado/Proyecto Semaforo/Proyecto Semaforo.srcs/sources_1/new/main.vhd
+++ Vivado/Proyecto Semaforo/Proyecto Semaforo.srcs/sources_1/new/main.vhd
@@ -32,12 +32,71 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity main is
---  Port ( );
+Port (
+    Clock : in STD_LOGIC;
+    semaforo : out STD_LOGIC_VECTOR(2 downto 0) := "100"
+);
 end main;
 
 architecture Behavioral of main is
+signal Clock_1s: std_logic:= '0';
+signal Clock_Hs: std_logic:= '0';
+
+signal contador: integer := 0;
+signal color : std_logic_vector(1 downto 0) := "00";
+signal contParpadeo : integer := 0;
 
 begin
 
+
+New_Clock : Process(Clock)
+variable Counter: integer:= 1;
+ begin
+ if rising_edge(Clock) then
+  
+    if Counter = 60000000 then
+        Clock_1s <= not Clock_1s;
+        Counter :=1;
+    else
+        Counter:= Counter +1;
+     end if; 
+ 
+ end if;
+ end process New_Clock;
+ 
+ New_Clock2 : Process(Clock)
+variable Counter: integer:= 1;
+ begin
+ if rising_edge(Clock) then
+  
+    if Counter = 30000000 then
+        Clock_Hs <= not Clock_Hs;
+        Counter :=1;
+    else
+        Counter:= Counter +1;
+     end if; 
+ 
+ end if;
+ end process New_Clock2;
+ 
+ process (Clock_1s)
+    begin
+        if rising_edge(Clock_1s) then
+            contador <= contador + 1;
+            if contador = 5 and color = "00" then
+                color <= "01";
+                contador <= 0;
+                semaforo <= "010";
+            elsif contador = 3 and color = "01" then
+                color <= "10";
+                contador <= 0;
+                semaforo <= "001";
+            elsif contador = 4 and color = "10" then
+                color <= "00";
+                contador <= 0;
+                semaforo <= "100";
+            end if;
+        end if;
+    end process;
 
 end Behavioral;
